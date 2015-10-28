49dc1ab7519015d3bd330ca2071fbe24444803a8
xtian <christian.bourgeois@socgen.com>
2015-10-28 12:40:33 +0100

    scripts

--- xtra/rhel7/install.sh
+++ xtra/rhel7/install.sh
@@ -5,32 +5,35 @@ if [ "$1" == "-d" ]; then
    trap read debug
 fi
 
-#get-path
-SOURCE=$0
-SOURCE=$( dirname $SOURCE )
-SOURCE=$( realpath $SOURCE )
-SOURCE=$( dirname $SOURCE )
-
 #################################################################
 #root part
 #################################################################
+
 if [ $EUID -eq 0 ]; then
-   #install packages
-   yum install rpm-build mock
 
-   #create user makerpm
-   id makerpm &> /dev/null || (
-      useradd -G mock,wheel makerpm
-      echo makerpm | passwd makerpm --stdin
-   )
-   
-   
-   #create rhel mock template
-   cp $SOURCE/install/rhel.cfg /etc/mock/
+	#install epel repository
+	yum install -y wget
+	if [ ! -f epel-release-latest-7.noarch.rpm ]; then
+		wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+		rpm -Uvh epel-release-7*.rpm
+	fi
 
-   echo "use 'su - makerpm', copy folder, then restart this script to continue"
-   
-   exit
+	#install packages
+	yum install -y rpm-build mock
+
+	#create rhel mock template
+	cp rhel.cfg /etc/mock/
+	
+	#create user makerpm
+	id makerpm &> /dev/null || (
+		useradd -G mock,wheel makerpm
+		echo makerpm | passwd makerpm --stdin
+	)
+	
+	echo "password for makerpm user is: makerpm"
+	
+	exit
+	
 fi
 
 #################################################################
